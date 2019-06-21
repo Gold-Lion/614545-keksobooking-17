c4d6971a3888013e1b75d73487b31066f5ff3e0d
Igor <igor.lavrinoff@yandex.ru>
2019-06-21 23:49:34 +0400

    Задание DOM

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var TOTAL_PINS = 8;
 var PIN_WIDTH = 40;
-var PIN_HEIGHT = 84;
+var PIN_HEIGHT = 40;
 var MAP_WIDTH = 1200;
 var MIN_X = 0;
 var MAX_X = MAP_WIDTH - PIN_WIDTH / 2;
 var MIN_Y = 130;
 var MAX_Y = 630;
 var map = document.querySelector(".map");
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
 map.classList.remove("map--faded");
 
@@ -41,3 +42,32 @@ var addAds = function () {
   };
   return ads;
 };
+
+var renderPin = function (adsd) {
+  var pinElement = pinTemplate.cloneNode(true);
+  // var titleAds = document.querySelector('#title');
+
+  pinElement.style = 'left: ' + (adsd.location.x - PIN_WIDTH / 2) + 'px; top: ' + (adsd.location.y - PIN_HEIGHT);
+  pinElement.querySelector('img').src = adsd.author.avatar;
+  // pinElement.querySelector('img').alt = titleAds.placeholder;
+
+  return pinElement;
+};
+
+var showAds = function () {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < TOTAL_PINS; i++) {
+    fragment.appendChild(renderPin(addAds[i]));
+  }
+
+  pinTemplate.appendChild(fragment);
+};
+
+showAds();
+/*
+<template id="pin">
+  <button type="button" class="map__pin" style="left: 200px; top: 400px;">
+    <img src="img/avatars/user07.png" width="40" height="40" draggable="false" alt="Метка объявления">
+  </button>
+</template>
+*/
