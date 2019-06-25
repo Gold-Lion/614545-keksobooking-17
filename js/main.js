1de52e7f84b58bb783d4a826e697c0bbc868054a
Igor <igor.lavrinoff@yandex.ru>
2019-06-26 00:37:27 +0400

    Выполнил задание DOM

--- js/main.js
+++ js/main.js
@@ -1,73 +1,80 @@
+'use strict';
+
 var HOUSE_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var TOTAL_PINS = 8;
-var PIN_WIDTH = 40;
-var PIN_HEIGHT = 40;
+var PIN_WIDTH = 50;
 var MAP_WIDTH = 1200;
 var MIN_X = 0;
-var MAX_X = MAP_WIDTH - PIN_WIDTH / 2;
+var MAX_X = MAP_WIDTH - PIN_WIDTH;
 var MIN_Y = 130;
 var MAX_Y = 630;
-var map = document.querySelector(".map");
+var map = document.querySelector('.map');
+var mapPins = map.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-map.classList.remove("map--faded");
+var activateMap = function () {
+  map.classList.remove('map--faded');
+};
 
-var getRandomAvatar = function (array) {
-  for (var i = 1; i <= array.length; i++) {
-    var imageSrc = 'img/avatars/user' + (i < 10 ? '0' + i : i) + '.png';
+var getAvatar = function (number) {
+  if (number > TOTAL_PINS || number < 1) {
+    number = TOTAL_PINS;
   }
-  return imageSrc;
+  return 'img/avatars/user' + (number < 10 ? '0' + number : number) + '.png';
 };
 
+// Получаем случайные координаты для элементов
 var getRandomElement = function (minValue, maxValue) {
-  return Math.floor(Math.random() * (maxValue - minValue)) + minValue;
+  return Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
 };
 
+// Получаем случайные элемент из массива с типом жилья
 var getRandomArrayElement = function (types) {
-  return types[getRandomElement(0, types.length)];
+  return types[getRandomElement(0, types.length - 1)];
 };
 
-var addAds = function () {
-  var ads = {
+// Получаем случайные объявления
+var getRandomAd = function (number) {
+  var ad = {
     author: {
-      avatar: getRandomAvatar(TOTAL_PINS)
+      avatar: getAvatar(number)
     },
     offer: {
       type: getRandomArrayElement(HOUSE_TYPES)
     },
     location: {
-      x: getRandomElement(MIN_X, MAX_X + 1),
-      y: getRandomElement(MIN_Y, MAX_Y + 1)
+      x: getRandomElement(MIN_X, MAX_X),
+      y: getRandomElement(MIN_Y, MAX_Y)
     }
   };
+  return ad;
+};
+
+var getRandomAds = function (count) {
+  var ads = [];
+
+  for (var i = 0; i < count; i++) {
+    ads[i] = getRandomAd(i + 1);
+  }
   return ads;
 };
 
-var renderPin = function (adsd) {
-  var pinElement = pinTemplate.cloneNode(true);
-  // var titleAds = document.querySelector('#title');
-
-  pinElement.style = 'left: ' + (adsd.location.x - PIN_WIDTH / 2) + 'px; top: ' + (adsd.location.y - PIN_HEIGHT);
-  pinElement.querySelector('img').src = adsd.author.avatar;
-  // pinElement.querySelector('img').alt = titleAds.placeholder;
-
-  return pinElement;
-};
-
-var showAds = function () {
+var showAds = function (ads) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < TOTAL_PINS; i++) {
-    fragment.appendChild(renderPin(addAds[i]));
-  }
 
-  pinTemplate.appendChild(fragment);
+  for (var i = 0; i < ads.length; i++) {
+    var pinElement = pinTemplate.cloneNode(true);
+    var titleAds = document.querySelector('#title');
+
+    var ad = ads[i];
+
+    pinElement.style = 'left: ' + (ad.location.x) + 'px; top: ' + (ad.location.y) + 'px;';
+    pinElement.querySelector('img').src = ad.author.avatar;
+    pinElement.querySelector('img').alt = titleAds.placeholder;
+    fragment.appendChild(pinElement);
+  }
+  mapPins.appendChild(fragment);
 };
 
-showAds();
-/*
-<template id="pin">
-  <button type="button" class="map__pin" style="left: 200px; top: 400px;">
-    <img src="img/avatars/user07.png" width="40" height="40" draggable="false" alt="Метка объявления">
-  </button>
-</template>
-*/
+activateMap();
+showAds(getRandomAds(TOTAL_PINS));
